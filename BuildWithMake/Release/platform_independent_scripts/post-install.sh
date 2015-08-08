b3d061e44020238597ce18d376bc0db252df18b3
nwilson <nwilson@osmsc.com>
2015-08-08 21:27:15 +0000

    now adding xfsv to launch xf on linux platforms using SV executable

--- BuildWithMake/Release/platform_independent_scripts/post-install.sh
+++ BuildWithMake/Release/platform_independent_scripts/post-install.sh
@@ -5,17 +5,20 @@ rm -f /usr/local/bin/adaptorREPLACE_SIMVASCULAR_POSTFIX
 rm -f /usr/local/bin/tetadaptorREPLACE_SIMVASCULAR_POSTFIX
 rm -f /usr/local/bin/presolverREPLACE_SIMVASCULAR_POSTFIX
 rm -f /usr/local/bin/postsolverREPLACE_SIMVASCULAR_POSTFIX
+rm -f /usr/local/bin/xfsvREPLACE_SIMVASCULAR_POSTFIX
 cp /usr/local/package/REPLACE_SIMVASCULAR_VERSION/REPLACE_TIMESTAMP/generic_launch_script /usr/local/bin/REPLACE_SIMVASCULAR_VERSION
 cp /usr/local/package/REPLACE_SIMVASCULAR_VERSION/REPLACE_TIMESTAMP/generic_launch_script /usr/local/bin/REPLACE_SIMVASCULAR_VERSION-cmdline
 cp /usr/local/package/REPLACE_SIMVASCULAR_VERSION/REPLACE_TIMESTAMP/generic_launch_script /usr/local/bin/adaptorREPLACE_SIMVASCULAR_POSTFIX
 cp /usr/local/package/REPLACE_SIMVASCULAR_VERSION/REPLACE_TIMESTAMP/generic_launch_script /usr/local/bin/tetadaptorREPLACE_SIMVASCULAR_POSTFIX
 cp /usr/local/package/REPLACE_SIMVASCULAR_VERSION/REPLACE_TIMESTAMP/generic_launch_script /usr/local/bin/presolverREPLACE_SIMVASCULAR_POSTFIX
 cp /usr/local/package/REPLACE_SIMVASCULAR_VERSION/REPLACE_TIMESTAMP/generic_launch_script /usr/local/bin/postsolverREPLACE_SIMVASCULAR_POSTFIX
+cp /usr/local/package/REPLACE_SIMVASCULAR_VERSION/REPLACE_TIMESTAMP/generic_launch_script /usr/local/bin/xfsvREPLACE_SIMVASCULAR_POSTFIX
 chmod a+rx /usr/local/bin/simvascularREPLACE_SIMVASCULAR_POSTFIX
 chmod a+rx /usr/local/bin/simvascularREPLACE_SIMVASCULAR_POSTFIX-cmdline
 chmod a+rx /usr/local/bin/adaptorREPLACE_SIMVASCULAR_POSTFIX
 chmod a+rx /usr/local/bin/tetadaptorREPLACE_SIMVASCULAR_POSTFIX
 chmod a+rx /usr/local/bin/presolverREPLACE_SIMVASCULAR_POSTFIX
 chmod a+rx /usr/local/bin/postsolverREPLACE_SIMVASCULAR_POSTFIX
+chmod a+rx /usr/local/bin/xfsvREPLACE_SIMVASCULAR_POSTFIX
 
 
