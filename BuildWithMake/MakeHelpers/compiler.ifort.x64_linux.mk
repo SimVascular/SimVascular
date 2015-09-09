142ff137fb0a2727099fdc73e63fad8d58a3d2f5
osmsc <osmsc@users.noreply.github.com>
2015-09-09 14:42:39 -0700

    files that were missed during commit

---
+++ BuildWithMake/MakeHelpers/compiler.ifort.x64_linux.mk
@@ -0,0 +1,22 @@
+ifeq ($(CLUSTER), x64_linux)
+    F90             = ifort -threads -fpp
+ifeq ($(MAKE_FULLY_OPTIMIZED),1)
+    OPT_FFLAGS      = -O3 -fPIC
+    DEBUG_FFLAGS    =
+else
+  ifeq ($(MAKE_OPTIMIZED),1)
+    OPT_FFLAGS      = -O2 -fPIC -g
+    DEBUG_FFLAGS    =
+  else
+    OPT_FFLAGS      =
+    DEBUG_FFLAGS    = -O0 -fPIC -debug -g -check -traceback 
+  endif
+endif
+    GLOBAL_FFLAGS   = $(BUILDFLAGS) $(DEBUG_FFLAGS) $(OPT_FFLAGS) \
+                      -W0 -132 -heap-arrays 16 
+ifeq ($(LINK_WITH_DEBUG),1)
+    GLOBAL_LFLAGS   += -g
+endif  
+#    F90_LIBS        = -lifcoremt -lifport -limf -lintlc -ldl
+    F90_LIBS        = -lifcoremt -lifport -limf
+endif
