e0fa8dd05e6b6746b86d160d2372f880155050d2
osmsc <osmsc@users.noreply.github.com>
2015-09-09 14:54:39 -0700

    added mkdir to mpi dir build rules

--- BuildWithMake/MakeHelpers/rules.x64_linux.mk
+++ BuildWithMake/MakeHelpers/rules.x64_linux.mk
@@ -32,15 +32,19 @@ $(BUILD_DIR)/%.o: %.f90
 	$(F90) $(FFLAGS) -c $< -o $@
 
 $(BUILD_MPI_DIR)/%.o: %.cxx
+	mkdir -p $(dir $@)
 	$(CXX) $(CXXFLAGS) -c $< -o $@
 
 $(BUILD_MPI_DIR)/%.o: %.c
+	mkdir -p $(dir $@)
 	$(CC) $(CCFLAGS) -c $< -o $@
 
 $(BUILD_MPI_DIR)/%.o: %.f
+	mkdir -p $(dir $@)
 	$(F90) $(FFLAGS) -c $< -o $@
 
 $(BUILD_MPI_DIR)/%.o: %.f90
+	mkdir -p $(dir $@)
 	$(F90) $(FFLAGS) -c $< -o $@
 
 endif
