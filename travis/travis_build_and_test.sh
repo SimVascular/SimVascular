e8a954d0888b205271e6f98fa634fab1ca08570a
Jameson Merkow <jmerkow@gmail.com>
2015-09-09 17:38:03 -0700

    add scripts to run test during ci

--- travis/travis_build_and_test.sh
+++ travis/travis_build_and_test.sh
@@ -2,22 +2,27 @@
 
 set -e
 MAKE="make --jobs=$NUM_THREADS --keep-going"
-
+MAKE_TEST="xvfb-run -a make test ARGS=-V"
 
 if $WITH_CMAKE; then
   mkdir -p $BUILD_DIR
   cd $BUILD_DIR
   CMAKE_BUILD_ARGS=""
-    if $PREBUILD_ITKVTK; then
-        CMAKE_BUILD_ARGS="$CMAKE_BUILD_ARGS -DSimVascular_USE_SYSTEM_VTK:BOOL=1 -DSimVascular_USE_SYSTEM_ITK:BOOL=1 -DVTK_DIR:PATH=$VTK_DIR -DITK_DIR:PATH=$ITK_DIR"
-    fi
-    if $BUILD_SOLVER; then
-        CMAKE_BUILD_ARGS="$CMAKE_BUILD_ARGS -DBUILD_ThreeDSolver=1 -DSimVascular_USE_DUMMY_MPICH2=1"
-    fi
-
+  if $PREBUILD_ITKVTK; then
+    CMAKE_BUILD_ARGS="$CMAKE_BUILD_ARGS -DSimVascular_USE_SYSTEM_VTK:BOOL=1 -DSimVascular_USE_SYSTEM_ITK:BOOL=1 -DVTK_DIR:PATH=$VTK_DIR -DITK_DIR:PATH=$ITK_DIR"
+  fi
+  if $BUILD_SOLVER; then
+    CMAKE_BUILD_ARGS="$CMAKE_BUILD_ARGS -DBUILD_ThreeDSolver=1 -DSimVascular_USE_DUMMY_MPICH2=1"
+  fi
+  if $BUILD_TEST; then
+     CMAKE_BUILD_ARGS="$CMAKE_BUILD_ARGS -DBUILD_TESTING:BOOL=1  -DSimVascular_TEST_DIR:PATH=$SV_TEST_DIR/automated_tests -DSimVascular_RUN_GUI_TESTS:BOOL=0 " 
+  fi
   echo CMAKE_BUILD_ARGS: $CMAKE_BUILD_ARGS
   cmake $CMAKE_BUILD_ARGS ../Code
-  $MAKE  
+  $MAKE
+  if $BUILD_TEST; then
+    $MAKE_TEST
+  fi
   $MAKE clean
   cd -
 
