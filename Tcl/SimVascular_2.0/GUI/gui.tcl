7c14278ec65861a92111509f4d25135b3880bcbd
Jameson Merkow <jmerkow@gmail.com>
2015-12-01 17:05:50 -0800

    no renderer default to 0

--- Tcl/SimVascular_2.0/GUI/gui.tcl
+++ Tcl/SimVascular_2.0/GUI/gui.tcl
@@ -46583,9 +46583,7 @@ proc mainGUI {} {
   global guiTRIMvars
   global gRen3d
   global SIMVASCULAR_NO_RENDERER
-  if { $SIMVASCULAR_NO_RENDERER == "0" } {
-    puts "No render window"
-  } else {
+  if { $SIMVASCULAR_NO_RENDERER != "1" } {
     set gRen3d [vis_gRenWin_3D]
   }
 
