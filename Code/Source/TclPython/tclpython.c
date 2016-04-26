3b423a5b3203a696262e1133e03c1488d889afd4
Adam Updegrove <updega2@gmail.com>
2016-04-26 16:17:45 -0700

    Build info tells when executable was built

--- Code/Source/TclPython/tclpython.c
+++ Code/Source/TclPython/tclpython.c
@@ -184,10 +184,10 @@ static int newInterpreter(Tcl_Interp *interpreter)
     Tcl_SetHashValue(Tcl_CreateHashEntry(&threadStates, (ClientData)identifier, &created), 0);
 #else
     if (existingInterpreters == 0) {
+	//Py_SetProgramName("/Users/adamupdegrove/Documents/Software/SimVascular/MyMaster/Code/PyCMakeBuild/externals/PYTHON/bin/python");
         Py_Initialize();                                                                           /* initialize main interpreter */
-	//Py_SetProgramName("/Users/adamupdegrove/Documents/Software/SimVascular/MyMaster/Code/MyPyBuild/externals/PYTHON/Python.framework/Versions/2.7/bin/python");
+	fprintf(stdout,"Check %s\n",Py_GetBuildInfo());
 	//Py_SetPath("/Users/adamupdegrove/Documents/Software/SimVascular/MyMaster/Code/MyPyBuild/externals/PYTHON/Python.framework/Versions/2.7/bin");
-	fprintf(stdout,"Check %s\n",Py_GetProgramName());
 	PyEval_InitThreads();                                               /* initialize and acquire the global interpreter lock */
         PycString_IMPORT;
         globalState = PyThreadState_Swap(0);                                                            /* save the global thread */
