5620bdd52b54a5eeaba6a784dcb025dc0bc60f66
Adam Updegrove <updega2@gmail.com>
2016-05-04 10:25:00 -0700

    Made FindMMG work for USE_SYSTEM

--- Code/CMake/FindMMG.cmake
+++ Code/CMake/FindMMG.cmake
@@ -59,9 +59,12 @@ FIND_LIBRARY(MMG_LIBRARY
   DOC "The mmg library"
   )
 
+set(MMG_DIR "" CACHE PATH "Path to top level libraries.  Specify this if MMG cannot be found.")
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(MMG DEFAULT_MSG
-  MMG_INCLUDE_DIR MMG_LIBRARY)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(MMG 
+	FOUND_VAR MMG_FOUND
+	REQUIRED_VARS MMG_INCLUDE_DIR MMG_LIBRARY
+	FAIL_MESSAGE "Could NOT find MMG")
 
 #IF ( NOT MMG_FOUND )
 #   MESSAGE (WARNING "\nMMG not found: you can set the 'MMG_DIR' environment"
