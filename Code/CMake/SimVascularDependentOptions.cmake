97e5b70732975c719cafe726c20aa18dcb945541
Adam Updegrove <updega2@gmail.com>
2016-06-08 19:34:25 -0700

    try the other c++11 flag

--- Code/CMake/SimVascularDependentOptions.cmake
+++ Code/CMake/SimVascularDependentOptions.cmake
@@ -167,8 +167,8 @@ if(SV_USE_OpenCASCADE_SHARED)
 endif()
 
 if(SV_USE_VTK_SHARED)
-  if(UNIX AND NOT APPLE)
-  	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=gnu++11")
+  if(LINUX)
+  	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
   endif()
 endif()
 
