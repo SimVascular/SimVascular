26291760837df61632e304b95b2b9689428f39d2
Adam Updegrove <updega2@gmail.com>
2016-07-28 08:09:27 -0700

    DEV: Adding cmake script for mac

---
+++ BuildWithMake/TestWithCMake/cmake-build-mac-noflowsolver-allexternals-relwdebinfo.sh
@@ -0,0 +1,55 @@
+rm -Rf ../../Code/ScriptBuild
+mkdir -p ../../Code/ScriptBuild
+pushd ../../Code/ScriptBuild
+
+export CC=clang
+export CXX=clang++
+
+export REPLACEME_SV_CMAKE_CMD="cmake"
+export REPLACEME_SV_CMAKE_GENERATOR="Unix Makefiles"
+export REPLACEME_SV_CMAKE_BUILD_TYPE="RelWithDebInfo"
+export REPLACEME_SV_MAKE_CMD="make -j8"
+export REPLACEME_SV_TOP_SRC_DIR_SV="../"
+
+"$REPLACEME_SV_CMAKE_CMD" \
+\
+    --debug-output \
+\
+   -G "$REPLACEME_SV_CMAKE_GENERATOR" \
+\
+   -DCMAKE_BUILD_TYPE="$REPLACEME_SV_CMAKE_BUILD_TYPE" \
+\
+   -DSV_SUPERBUILD=OFF \
+\
+   -DSV_USE_FREETYPE=ON \
+   -DSV_USE_GDCM=ON \
+   -DSV_USE_ITK=ON \
+   -DSV_USE_MPICH2=OFF \
+   -DSV_USE_OpenCASCADE=ON \
+   -DSV_USE_PYTHON=ON \
+   -DSV_USE_MMG=ON \
+\
+   -DSV_USE_SYSTEM_FREETYPE=ON \
+   -DSV_USE_SYSTEM_GDCM=ON \
+   -DSV_USE_SYSTEM_ITK=ON \
+   -DSV_USE_SYSTEM_PYTHON=ON \
+   -DSV_USE_SYSTEM_OPENCASCADE=ON \
+   -DSV_USE_SYSTEM_TCL=ON \
+   -DSV_USE_SYSTEM_VTK=ON \
+   -DSV_USE_SYSTEM_MMG=ON \
+\
+   -DSV_USE_GDCM_SHARED=ON \
+   -DSV_USE_FREETYPE_SHARED=ON \
+   -DSV_USE_ITK_SHARED=ON \
+   -DSV_USE_OpenCASCADE_SHARED=ON \
+   -DSV_USE_TCL_SHARED=ON \
+   -DSV_USE_VTK_SHARED=ON \
+\
+   -DSV_EXTERNALS_USE_TOPLEVEL_DIR=ON \
+   -DSV_EXTERNALS_TOPLEVEL_DIR="/Users/adamupdegrove/Documents/Software/SimVascular/MyMaster/Code/MasterBuild/Externals" \
+\
+ "$REPLACEME_SV_TOP_SRC_DIR_SV" >& stdout-cmake-config.txt
+
+$REPLACEME_SV_MAKE_CMD >& stdout-compile.txt
+
+popd
