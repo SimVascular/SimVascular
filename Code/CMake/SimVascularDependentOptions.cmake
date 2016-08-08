bf7363ea616a5083442affd7fda209b8e0afeaf7
Adam Updegrove <updega2@gmail.com>
2016-08-08 15:44:26 -0700

    MAINT: Put it back to c++11 flag

--- Code/CMake/SimVascularDependentOptions.cmake
+++ Code/CMake/SimVascularDependentOptions.cmake
@@ -168,7 +168,7 @@ endif()
 
 if(SV_USE_VTK_SHARED)
   if(LINUX)
-  	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=gnu++11")
+  	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
   endif()
 endif()
 
