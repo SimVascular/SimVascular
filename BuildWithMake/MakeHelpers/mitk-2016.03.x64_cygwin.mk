9548120fd0dc591ceca948120e785dd4472f536c
osmsc <nwilson@osmsc.com>
2016-08-15 00:32:29 -0700

    cleaning up paths for mitk

--- BuildWithMake/MakeHelpers/mitk-2016.03.x64_cygwin.mk
+++ BuildWithMake/MakeHelpers/mitk-2016.03.x64_cygwin.mk
@@ -2,9 +2,7 @@
 # MITK
 # ----
 
-MITK_SRCDIR = $(OPEN_SOFTWARE_SOURCES_TOPLEVEL)/mitk-2016.03
 MITK_BINDIR = $(OPEN_SOFTWARE_BINARIES_TOPLEVEL)/mitk-2016.03
-MITK_BLDDIR = C:/mitk14
 
 MITK_LIBDIRS = $(MITK_BINDIR)/lib
 MITK_BINDIRS = $(MITK_BINDIR)/bin
@@ -20,9 +18,10 @@ MITK_DEFS = -D_WIN32_WINNT=0x0A00 -DUS_MODULE_NAME=svlib
 
 MITK_INCDIRS = \
            -I$(MITK_BINDIR)/include \
-           -I$(MITK_BINDIR)/include/ctk-0.1 \
+           -I$(MITK_BINDIR)/include/ctk \
            -I$(MITK_BINDIR)/include/eigen3 \
            -I$(MITK_BINDIR)/include/mitk \
+           -I$(MITK_BINDIR)/include/mitk/configs \
            -I$(MITK_BINDIR)/include/mitk/exports \
            -I$(MITK_BINDIR)/include/mitk/ui_files \
            -I$(MITK_BINDIR)/include/mitk/AlgorithmsExt \
@@ -46,41 +45,33 @@ MITK_INCDIRS = \
            -I$(MITK_BINDIR)/include/mitk/Utilities/mbilog \
            -I$(MITK_BINDIR)/include/PythonQt \
            -I$(MITK_BINDIR)/include/tinyxml \
-           -I$(MITK_SRCDIR)/Modules/ContourModel/DataManagement \
-           -I$(MITK_SRCDIR)/Modules/CppMicroServices/core/src/module \
-           -I$(MITK_SRCDIR)/Modules/CppMicroServices/core/src/service \
-           -I$(MITK_SRCDIR)/Modules/CppMicroServices/core/src/util \
-           -I$(MITK_SRCDIR)/Modules/ImageDenoising \
-           -I$(MITK_SRCDIR)/Modules/LegacyGL \
-           -I$(MITK_SRCDIR)/Modules/Multilabel \
-           -I$(MITK_SRCDIR)/Modules/Overlays \
-           -I$(MITK_SRCDIR)/Modules/Segmentation/Algorithms \
-           -I$(MITK_SRCDIR)/Modules/Segmentation/Controllers \
-           -I$(MITK_SRCDIR)/Modules/Segmentation/Interactions \
-           -I$(MITK_SRCDIR)/Modules/SegmentationUI/Qmitk \
-           -I$(MITK_SRCDIR)/Modules/SurfaceInterpolation \
-           -I$(MITK_BLDDIR)/ep/src/CTK/Libs/Core \
-           -I$(MITK_BLDDIR)/ep/src/CTK/Libs/Scripting/Python/Core/ \
-           -I$(MITK_BLDDIR)/ep/src/CTK/Libs/Scripting/Python/Widgets \
-           -I$(MITK_BLDDIR)/ep/src/CTK/Libs/Visualization/VTK/Core \
-           -I$(MITK_BLDDIR)/ep/src/CTK/Libs/Widgets \
-           -I$(MITK_BLDDIR)/MITK-build/Modules/ContourModel \
-           -I$(MITK_BLDDIR)/MITK-build/Modules/ImageDenoising \
-           -I$(MITK_BLDDIR)/MITK-build/Modules/LegacyGL \
-           -I$(MITK_BLDDIR)/MITK-build/Modules/Multilabel \
-           -I$(MITK_BLDDIR)/MITK-build/Modules/Overlays \
-           -I$(MITK_BLDDIR)/MITK-build/Modules/QtWidgets \
-           -I$(MITK_BLDDIR)/MITK-build/Modules/Segmentation \
-           -I$(MITK_BLDDIR)/MITK-build/Modules/Segmentation/Interactions \
-           -I$(MITK_BLDDIR)/MITK-build/Modules/SegmentationUI \
-           -I$(MITK_BLDDIR)/MITK-build/Modules/SurfaceInterpolation \
-           -I$(MITK_SRCDIR)/Utilities/mbilog
+           -I$(MITK_BINDIR)/include/mitk/Modules/ContourModel/DataManagement \
+           -I$(MITK_BINDIR)/include/mitk/Modules/CppMicroServices/core/src/module \
+           -I$(MITK_BINDIR)/include/mitk/Modules/CppMicroServices/core/src/service \
+           -I$(MITK_BINDIR)/include/mitk/Modules/CppMicroServices/core/src/util \
+           -I$(MITK_BINDIR)/include/mitk/Modules/ImageDenoising \
+           -I$(MITK_BINDIR)/include/mitk/Modules/LegacyGL \
+           -I$(MITK_BINDIR)/include/mitk/Modules/Multilabel \
+           -I$(MITK_BINDIR)/include/mitk/Modules/Overlays \
+           -I$(MITK_BINDIR)/include/mitk/Modules/Segmentation/Algorithms \
+           -I$(MITK_BINDIR)/include/mitk/Modules/Segmentation/Controllers \
+           -I$(MITK_BINDIR)/include/mitk/Modules/Segmentation/Interactions \
+           -I$(MITK_BINDIR)/include/mitk/Modules/SegmentationUI/Qmitk \
+           -I$(MITK_BINDIR)/include/mitk/Modules/SurfaceInterpolation \
+           -I$(MITK_BINDIR)/include/mitk/Modules/ContourModel \
+           -I$(MITK_BINDIR)/include/mitk/Modules/ImageDenoising \
+           -I$(MITK_BINDIR)/include/mitk/Modules/LegacyGL \
+           -I$(MITK_BINDIR)/include/mitk/Modules/Multilabel \
+           -I$(MITK_BINDIR)/include/mitk/Modules/Overlays \
+           -I$(MITK_BINDIR)/include/mitk/Modules/QtWidgets \
+           -I$(MITK_BINDIR)/include/mitk/Modules/Segmentation \
+           -I$(MITK_BINDIR)/include/mitk/Modules/Segmentation/Interactions \
+           -I$(MITK_BINDIR)/include/mitk/Modules/SegmentationUI \
+           -I$(MITK_BINDIR)/include/mitk/Modules/SurfaceInterpolation \
+           -I$(MITK_BINDIR)/include/mitk/Utilities/mbilog
 
 MITK_LIBS = \
-           $(LIBPATH_COMPILER_FLAG)$(MITK_BINDIR)/bin \
            $(LIBPATH_COMPILER_FLAG)$(MITK_BINDIR)/lib \
-           $(LIBPATH_COMPILER_FLAG)$(MITK_BLDDIR)/ep/src/CTK-build/CTK-build/bin/Release \
-           $(LIBPATH_COMPILER_FLAG)$(MITK_BLDDIR)/MITK-build/lib/Release \
            $(LIBFLAG)CppMicroServices$(LIBLINKEXT) \
            $(LIBFLAG)CTKCommandLineModulesBackendLocalProcess$(LIBLINKEXT) \
            $(LIBFLAG)CTKCommandLineModulesCore$(LIBLINKEXT) \
