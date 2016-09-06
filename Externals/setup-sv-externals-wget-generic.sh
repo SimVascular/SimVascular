f89e7e2c531de62cc9a91164f44d70044d266692
osmsc <nwilson@osmsc.com>
2016-09-05 17:42:06 -0700

    added the scripts to setup and build externals

---
+++ Externals/setup-sv-externals-wget-generic.sh
@@ -0,0 +1,66 @@
+export PARENT_URL=http://simvascular.stanford.edu/downloads/public/externals/originals/
+
+mkdir Originals
+pushd Originals
+
+mkdir -p freetype
+pushd freetype
+wget $PARENT_URL/freetype/freetype-2.5.5.tar.gz
+wget $PARENT_URL/freetype/ft263.zip
+popd
+
+mkdir -p gdcm
+pushd gdcm
+wget $PARENT_URL/gdcm/gdcm-2.6.1.tar.gz
+popd
+
+mkdir -p itk
+pushd itk
+wget $PARENT_URL/itk/InsightToolkit-4.7.1.tar.gz
+popd
+
+mkdir -p mitk
+pushd mitk
+wget $PARENT_URL/mitk/mitk-v2016.03.0.tar.gz
+popd
+
+mkdir -p mmg
+pushd mmg
+wget $PARENT_URL/mmg/mmg-5.1.0.tar.gz
+popd
+
+mkdir -p numpy
+pushd numpy
+wget $PARENT_URL/numpy/numpy-1.11.1.tar.gz
+popd
+
+mkdir -p opencascade
+pushd opencascade
+wget $PARENT_URL/opencascade/opencascade-7.0.0.tgz
+wget $PARENT_URL/opencascade/Release_Notes_7.0.0.pdf
+popd
+
+mkdir -p python
+pushd python
+wget $PARENT_URL/python/get-pip.py
+wget $PARENT_URL/python/Python-2.7.11.tgz
+wget $PARENT_URL/python/python-2.7.11-cmakebuild.tar.gz
+popd
+
+mkdir -p tcltk
+pushd tcltk
+wget $PARENT_URL/tcltk/tcl8.5.18-src.tar.gz
+wget $PARENT_URL/tcltk/tcl8.6.4-src.tar.gz
+wget $PARENT_URL/tcltk/tcllib-1.17.tar.gz
+wget $PARENT_URL/tcltk/tk8.5.18-src.tar.gz
+wget $PARENT_URL/tcltk/tk8.6.4-src.tar.gz
+wget $PARENT_URL/tcltk/tklib-0.6.tar.tgz
+popd
+
+mkdir -p vtk
+pushd vtk
+wget $PARENT_URL/vtk/VTK-6.2.0.tar.gz
+wget $PARENT_URL/vtk/VTK-6.3.0.tar.gz
+popd
+
+popd
---
+++ Externals/setup-sv-externals-windows.sh
@@ -0,0 +1,38 @@
+EXTERNALS_TOP=/usr/local/sv
+EXTERNALS_BUILD_TOP=/cygdrive/c/sv
+
+#
+# initial setup
+#
+
+echo "Deleting previous build dir ($EXTERNALS_BUILD_TOP)"
+rm -Rf $EXTERNALS_BUILD_TOP
+mkdir $EXTERNALS_BUILD_TOP
+
+echo "Deleting previous src+bin dir ($EXTERNALS_TOP)"
+rm -Rf $EXTERNALS_TOP
+mkdir -p $EXTERNALS_TOP
+mkdir -p $EXTERNALS_TOP/src
+cp -Rf BuildHelpers $EXTERNALS_TOP/src
+
+#
+# wget the original src files
+#
+
+source setup-sv-externals-wget-generic.sh
+mv Originals $EXTERNALS_TOP/src/BuildHelpers
+
+#
+# let's do it
+#
+
+pushd $EXTERNALS_TOP/src/BuildHelpers
+
+echo "Starting build in ($EXTERNALS_TOP)..."
+echo "  note: see logs in stdout.superbuild.txt, tmp/stdout*, etc."
+echo "  note: see individual build logs as well"
+source Scripts/superbuild-windows-cl.sh >& stdout.superbuild.txt
+
+popd
+
+
