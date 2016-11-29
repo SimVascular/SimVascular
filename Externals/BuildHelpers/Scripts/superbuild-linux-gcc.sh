5a7558104974027ddf6b9354e5be8d85053dc261
osmsc <nwilson@osmsc.com>
2016-11-28 23:42:48 -0800

    BUILD: fixed bug in itk script BUILD: removed AppUtil from Modules build due to missing files on linux BUILD: updated scripts to let advanced users only build a subset of Externals

--- Externals/BuildHelpers/Scripts/superbuild-linux-gcc.sh
+++ Externals/BuildHelpers/Scripts/superbuild-linux-gcc.sh
@@ -1,9 +1,31 @@
 #
-# unpack all of the source code
+#  Create directories
 #
 
-source Scripts/untar-unzip-source-all.sh
 mkdir -p tmp
+mkdir -p tar_output
+mkdir -p zip_output
+
+#
+#  Build everything, but can manually override for advanced users
+#
+
+if [ -z "$SV_SUPER_OPTIONS" ]; then
+   echo "NOTE: SV_SUPER_OPTIONS defaulting to all possible options."
+   rm -Rf tar_output
+   mkdir -p tar_output
+   rm -Rf zip_output
+   mkdir -p zip_output
+   SV_SUPER_OPTIONS="UNTAR_UNZIP_ALL BUILD_TCL BUILD_PYTHON BUILD_NUMPY BUILD_FREETYPE BUILD_GDCM BUILD_VTK BUILD_ITK BUILD_OPENCASCADE BUILD_MMG BUILD_MITK BUILD_TAR_FILES_ALL BUILD_ZIP_FILES_ALL TAR_EVERYTHING ZIP_EVERYTHING"
+fi
+
+#
+# unpack all of the source code
+#
+if [[ $SV_SUPER_OPTIONS == *UNTAR_UNZIP_ALL* ]]; then
+  echo "UNTAR_UNZIP_ALL"
+  source Scripts/untar-unzip-source-all.sh
+fi
 
 #
 # make build scripts
@@ -11,94 +33,166 @@ mkdir -p tmp
 
 
 #  tcl/tk 8.6
-sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh CompileScripts/tcl-linux-generic.sh > tmp/compile.make.tcl.gcc.sh
-chmod a+rx ./tmp/compile.make.tcl.gcc.sh
+if [[ $SV_SUPER_OPTIONS == *BUILD_TCL* ]]; then
+  echo "BUILD_TCL"
+  sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh CompileScripts/tcl-linux-generic.sh > tmp/compile.make.tcl.gcc.sh
+  chmod a+rx ./tmp/compile.make.tcl.gcc.sh
+fi
 
 # python 2.7
-sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh CompileScripts/compile-cmake-python-linux.sh > tmp/compile.cmake.python.gcc.sh
-chmod a+rx ./tmp/compile.cmake.python.gcc.sh
+if [[ $SV_SUPER_OPTIONS == *BUILD_PYTHON* ]]; then
+  echo "BUILD_PYTHON"
+  sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh CompileScripts/compile-cmake-python-linux.sh > tmp/compile.cmake.python.gcc.sh
+  chmod a+rx ./tmp/compile.cmake.python.gcc.sh
+fi
 
 # numpy
-sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh CompileScripts/compile-python-numpy-linux.sh > tmp/compile.python.numpy-linux.sh
-chmod a+rx ./tmp/compile.python.numpy-linux.sh
+if [[ $SV_SUPER_OPTIONS == *BUILD_NUMPY* ]]; then
+  echo "BUILD_NUMPY"
+  sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh CompileScripts/compile-python-numpy-linux.sh > tmp/compile.python.numpy-linux.sh
+  chmod a+rx ./tmp/compile.python.numpy-linux.sh
+fi
 
 # freetype
-sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh CompileScripts/compile-cmake-freetype-generic.sh > tmp/compile.cmake.freetype.gcc.sh
-chmod a+rx ./tmp/compile.cmake.freetype.gcc.sh
+if [[ $SV_SUPER_OPTIONS == *BUILD_FREETYPE* ]]; then
+  echo "BUILD_FREETYPE"
+  sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh CompileScripts/compile-cmake-freetype-generic.sh > tmp/compile.cmake.freetype.gcc.sh
+  chmod a+rx ./tmp/compile.cmake.freetype.gcc.sh
+fi
 
 # gdcm
-sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh CompileScripts/compile-cmake-gdcm-generic.sh > tmp/compile.cmake.gdcm.gcc.sh
-chmod a+rx ./tmp/compile.cmake.gdcm.gcc.sh
+if [[ $SV_SUPER_OPTIONS == *BUILD_GDCM* ]]; then
+  echo "BUILD_GDCM"
+  sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh CompileScripts/compile-cmake-gdcm-generic.sh > tmp/compile.cmake.gdcm.gcc.sh
+  chmod a+rx ./tmp/compile.cmake.gdcm.gcc.sh
+fi
 
 # vtk
-sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh CompileScripts/compile-cmake-vtk-generic.sh > tmp/compile.cmake.vtk.gcc.sh
-chmod a+rx ./tmp/compile.cmake.vtk.gcc.sh
+if [[ $SV_SUPER_OPTIONS == *BUILD_VTK* ]]; then
+  echo "BUILD_VTK"
+  sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh CompileScripts/compile-cmake-vtk-generic.sh > tmp/compile.cmake.vtk.gcc.sh
+  chmod a+rx ./tmp/compile.cmake.vtk.gcc.sh
+fi
 
 # itk
-sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh CompileScripts/compile-cmake-itk-generic.sh > tmp/compile.cmake.itk.gcc.sh
-chmod a+rx ./tmp/compile.cmake.itk.gcc.sh
+if [[ $SV_SUPER_OPTIONS == *BUILD_ITK* ]]; then
+  echo "BUILD_ITK"
+  sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh CompileScripts/compile-cmake-itk-generic.sh > tmp/compile.cmake.itk.gcc.sh
+  chmod a+rx ./tmp/compile.cmake.itk.gcc.sh
+fi
 
 # opencascade
-sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh CompileScripts/compile-cmake-opencascade-generic.sh > tmp/compile.cmake.opencascade.gcc.sh
-chmod a+rx ./tmp/compile.cmake.opencascade.gcc.sh
+if [[ $SV_SUPER_OPTIONS == *BUILD_OPENCASCADE* ]]; then
+  echo "BUILD_OPENCASCADE"
+  sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh CompileScripts/compile-cmake-opencascade-generic.sh > tmp/compile.cmake.opencascade.gcc.sh
+  chmod a+rx ./tmp/compile.cmake.opencascade.gcc.sh
+fi
 
 # mmg
-sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh CompileScripts/compile-cmake-mmg-generic.sh > tmp/compile.cmake.mmg.gcc.sh
-chmod a+rx ./tmp/compile.cmake.mmg.gcc.sh
+if [[ $SV_SUPER_OPTIONS == *BUILD_MMG* ]]; then
+  echo "BUILD_MMG"
+  sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh CompileScripts/compile-cmake-mmg-generic.sh > tmp/compile.cmake.mmg.gcc.sh
+  chmod a+rx ./tmp/compile.cmake.mmg.gcc.sh
+fi
 
 # mitk
-sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh CompileScripts/compile-cmake-mitk-linux.sh > tmp/compile.cmake.mitk.gcc.sh
-chmod a+rx ./tmp/compile.cmake.mitk.gcc.sh
-sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh CompileScripts/post-install-mitk-linux.sh > tmp/post-install-mitk-linux.sh
-chmod a+rx ./tmp/post-install-mitk-linux.sh
+if [[ $SV_SUPER_OPTIONS == *BUILD_MITK* ]]; then
+  echo "BUILD_MITK"
+  sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh CompileScripts/compile-cmake-mitk-linux.sh > tmp/compile.cmake.mitk.gcc.sh
+  chmod a+rx ./tmp/compile.cmake.mitk.gcc.sh
+  sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh CompileScripts/post-install-mitk-linux.sh > tmp/post-install-mitk-linux.sh
+  chmod a+rx ./tmp/post-install-mitk-linux.sh
+fi
 
 # create script to create tar files
-sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh Scripts/create-archives-all.sh > tmp/create-archives-all.gcc.sh
-chmod a+rx ./tmp/create-archives-all.gcc.sh
+if [[ $SV_SUPER_OPTIONS == *BUILD_TAR_FILES_ALL* ]]; then
+  echo "BUILD_TAR_FILES_ALL"
+  sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh Scripts/create-archives-generic.sh > tmp/create-archives-all.gcc.sh
+  chmod a+rx ./tmp/create-archives-all.gcc.sh
+fi
 
 # create script to create zip files
-sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh Scripts/tar-to-zip-all.sh > tmp/tar-to-zip-all.gcc.sh
-chmod a+rx ./tmp/tar-to-zip-all.gcc.sh
+if [[ $SV_SUPER_OPTIONS == *BUILD_ZIP_FILES_ALL* ]]; then
+  echo "BUILD_ZIP_FILES_ALL"
+  sed -f CompileScripts/sed-script-x64_linux-options-gcc.sh Scripts/tar-to-zip-all.sh > tmp/tar-to-zip-all.gcc.sh
+  chmod a+rx ./tmp/tar-to-zip-all.gcc.sh
+fi
 
 #
 # compile code
 #
 
 #  tcl/tk 8.6
-./tmp/compile.make.tcl.gcc.sh >& ./tmp/stdout.tcl.txt
+if [[ $SV_SUPER_OPTIONS == *BUILD_TCL* ]]; then
+  echo "BUILD_TCL"
+  ./tmp/compile.make.tcl.gcc.sh >& ./tmp/stdout.tcl.txt
+fi
 
 # python 2.7
-./tmp/compile.cmake.python.gcc.sh >& ./tmp/stdout.python.gcc.txt
+if [[ $SV_SUPER_OPTIONS == *BUILD_PYTHON* ]]; then
+  echo "BUILD_PYTHON"
+  ./tmp/compile.cmake.python.gcc.sh >& ./tmp/stdout.python.gcc.txt
+fi
 
 # numpy
-./tmp/compile.python.numpy-linux.sh >& ./tmp/stdout.numpy.python.txt
+if [[ $SV_SUPER_OPTIONS == *BUILD_NUMPY* ]]; then
+  echo "BUILD_NUMPY"
+  ./tmp/compile.python.numpy-linux.sh >& ./tmp/stdout.numpy.python.txt
+fi
 
 # freetype
-./tmp/compile.cmake.freetype.gcc.sh >& ./tmp/stdout.freetype.gcc.txt
+if [[ $SV_SUPER_OPTIONS == *BUILD_FREETYPE* ]]; then
+  echo "BUILD_FREETYPE"
+  ./tmp/compile.cmake.freetype.gcc.sh >& ./tmp/stdout.freetype.gcc.txt
+fi
 
 # gdcm
-./tmp/compile.cmake.gdcm.gcc.sh >& ./tmp/stdout.gdcm.gcc.txt
+if [[ $SV_SUPER_OPTIONS == *BUILD_GDCM* ]]; then
+  echo "BUILD_GDCM"
+  ./tmp/compile.cmake.gdcm.gcc.sh >& ./tmp/stdout.gdcm.gcc.txt
+fi
 
 # vtk
-./tmp/compile.cmake.vtk.gcc.sh >& ./tmp/stdout.vtk.gcc.txt
+if [[ $SV_SUPER_OPTIONS == *BUILD_VTK* ]]; then
+  echo "BUILD_VTK"
+  ./tmp/compile.cmake.vtk.gcc.sh >& ./tmp/stdout.vtk.gcc.txt
+fi
 
 # itk
-./tmp/compile.cmake.itk.gcc.sh >& ./tmp/stdout.itk.gcc.txt
+if [[ $SV_SUPER_OPTIONS == *BUILD_ITK* ]]; then
+  echo "BUILD_ITK"
+  ./tmp/compile.cmake.itk.gcc.sh >& ./tmp/stdout.itk.gcc.txt
+fi
 
 # opencascade
-./tmp/compile.cmake.opencascade.gcc.sh >& ./tmp/stdout.opencascade.gcc.txt
+if [[ $SV_SUPER_OPTIONS == *BUILD_OPENCASCADE* ]]; then
+  echo "BUILD_OPENCASCADE"
+  ./tmp/compile.cmake.opencascade.gcc.sh >& ./tmp/stdout.opencascade.gcc.txt
+fi
 
 # mmg
-./tmp/compile.cmake.mmg.gcc.sh >& ./tmp/stdout.mmg.gcc.txt
+if [[ $SV_SUPER_OPTIONS == *BUILD_MMG* ]]; then
+  echo "BUILD_MMG"
+  ./tmp/compile.cmake.mmg.gcc.sh >& ./tmp/stdout.mmg.gcc.txt
+fi
 
 # mitk
-./tmp/compile.cmake.mitk.gcc.sh >& ./tmp/stdout.mitk.gcc.txt
-./tmp/post-install-mitk-linux.sh >& ./tmp/stdout.post-install-mitk-linux.txt
+if [[ $SV_SUPER_OPTIONS == *BUILD_MITK* ]]; then
+  echo "BUILD_MITK"
+  ./tmp/compile.cmake.mitk.gcc.sh >& ./tmp/stdout.mitk.gcc.txt
+  ./tmp/post-install-mitk-linux.sh >& ./tmp/stdout.post-install-mitk-linux.txt
+fi
 
 #
 # create tar files for distrution
 #
 
-./tmp/create-archives-all.gcc.sh >& ./tmp/stdout.create-archives-all.gcc.txt
+if [[ $SV_SUPER_OPTIONS == *BUILD_TAR_FILES_ALL* ]]; then
+  echo "BUILD_TAR_FILES_ALL"
+  ./tmp/create-archives-all.gcc.sh >& ./tmp/stdout.create-archives-all.gcc.txt
+fi
 
-./tmp/tar-to-zip-all.gcc.sh >& ./tmp/stdout.tar-to-zip-all.gcc.txt
+if [[ $SV_SUPER_OPTIONS == *BUILD_ZIP_FILES_ALL* ]]; then
+  echo "BUILD_ZIP_FILES_ALL"
+  ./tmp/tar-to-zip-all.gcc.sh >& ./tmp/stdout.tar-to-zip-all.gcc.txt
+fi
--- Externals/BuildHelpers/Scripts/untar-unzip-source-all.sh
+++ Externals/BuildHelpers/Scripts/untar-unzip-source-all.sh
@@ -1,71 +1,103 @@
 #
-#  untar tcl/tk
+#  tcl/tk
 #
 
-tar xvf Originals/tcltk/tcl8.5.18-src.tar.gz
-tar xvf Originals/tcltk/tk8.5.18-src.tar.gz  
-tar xvf Originals/tcltk/tcl8.6.4-src.tar.gz
-tar xvf Originals/tcltk/tk8.6.4-src.tar.gz
-tar xvf Originals/tcltk/tcllib-1.17.tar.gz
-tar xvf Originals/tcltk/tklib-0.6.tar.tgz
+if [[ $SV_SUPER_OPTIONS == *BUILD_TCL* ]]; then
+  echo "BUILD_TCL"
 
-#
-# move and rename tcl/tk
-#
+  #  untar tcl/tk
+  tar xvf Originals/tcltk/tcl8.5.18-src.tar.gz
+  tar xvf Originals/tcltk/tk8.5.18-src.tar.gz  
+  tar xvf Originals/tcltk/tcl8.6.4-src.tar.gz
+  tar xvf Originals/tcltk/tk8.6.4-src.tar.gz
+  tar xvf Originals/tcltk/tcllib-1.17.tar.gz
+  tar xvf Originals/tcltk/tklib-0.6.tar.tgz
 
-mv tcl8.5.18 ../tcl-8.5.18
-mv tk8.5.18 ../tk-8.5.18
-mv tcl8.6.4 ../tcl-8.6.4
-mv tk8.6.4 ../tk-8.6.4
-mv tcllib-1.17 ../tcllib-1.17
-mv tklib-0.6 ../tklib-0.6
+  #
+  # move and rename tcl/tk
+  #
+
+  mv tcl8.5.18 ../tcl-8.5.18
+  mv tk8.5.18 ../tk-8.5.18
+  mv tcl8.6.4 ../tcl-8.6.4
+  mv tk8.6.4 ../tk-8.6.4
+  mv tcllib-1.17 ../tcllib-1.17
+  mv tklib-0.6 ../tklib-0.6
+
+  source Patches/patch-source-tcltk-8.5.sh
+
+fi
 
 #  python
-rm -Rf ../python-2.7.11
-tar xvf Originals/python/python-2.7.11-cmakebuild.tar.gz
-mv python-2.7.11-cmakebuild ../python-2.7.11
-tar xvf Originals/python/Python-2.7.11.tgz
-mv Python-2.7.11 ../python-2.7.11
-
-#  gdcm
-tar xvf Originals/gdcm/gdcm-2.6.1.tar.gz
-mv gdcm-2.6.1 ..
-
-# freetype2
-unzip Originals/freetype/ft263.zip
-mv freetype-2.6.3 ..
-
-# vtk
-tar xvf Originals/vtk/VTK-6.2.0.tar.gz
-mv VTK-6.2.0 ../vtk-6.2.0
-tar xvf Originals/vtk/VTK-6.3.0.tar.gz
-mv VTK-6.3.0 ../vtk-6.3.0
-
-# itk
-tar xvf Originals/itk/InsightToolkit-4.7.1.tar.gz
-mv InsightToolkit-4.7.1 ../itk-4.7.1
-
-# opencascade
-tar xvf Originals/opencascade/opencascade-7.0.0.tgz
-mv opencascade-7.0.0 ..
-
-# mmg
-tar xvf Originals/mmg/mmg-5.1.0.tar.gz
-mv mmg-5.1.0 ..
+if [[ $SV_SUPER_OPTIONS == *BUILD_PYTHON* ]]; then
+  echo "BUILD_PYTHON"
+  rm -Rf ../python-2.7.11
+  tar xvf Originals/python/python-2.7.11-cmakebuild.tar.gz
+  mv python-2.7.11-cmakebuild ../python-2.7.11
+  tar xvf Originals/python/Python-2.7.11.tgz
+  mv Python-2.7.11 ../python-2.7.11
+fi
 
 # numpy
-tar xvf Originals/numpy/numpy-1.11.1.tar.gz
-mv numpy-1.11.1 ..
+if [[ $SV_SUPER_OPTIONS == *BUILD_NUMPY* ]]; then
+  echo "BUILD_NUMPY"
+  tar xvf Originals/numpy/numpy-1.11.1.tar.gz
+  mv numpy-1.11.1 ..
+fi
+
+# freetype2
+if [[ $SV_SUPER_OPTIONS == *BUILD_FREETYPE* ]]; then
+  echo "BUILD_FREETYPE"
+  unzip Originals/freetype/ft263.zip
+  mv freetype-2.6.3 ..
+fi
+
+#  gdcm
+if [[ $SV_SUPER_OPTIONS == *BUILD_GDCM* ]]; then
+  echo "BUILD_GDCM"
+  tar xvf Originals/gdcm/gdcm-2.6.1.tar.gz
+  mv gdcm-2.6.1 ..
+fi
+
+# vtk
+if [[ $SV_SUPER_OPTIONS == *BUILD_VTK* ]]; then
+  echo "BUILD_VTK"
+  tar xvf Originals/vtk/VTK-6.2.0.tar.gz
+  mv VTK-6.2.0 ../vtk-6.2.0
+  tar xvf Originals/vtk/VTK-6.3.0.tar.gz
+  mv VTK-6.3.0 ../vtk-6.3.0
+  source Patches/patch-source-vtk-6.2.sh
+  source Patches/patch-source-vtk-6.3.sh
+fi
+
+# itk
+if [[ $SV_SUPER_OPTIONS == *BUILD_ITK* ]]; then
+  echo "BUILD_ITK"
+  tar xvf Originals/itk/InsightToolkit-4.7.1.tar.gz
+  mv InsightToolkit-4.7.1 ../itk-4.7.1
+fi
+
+# opencascade
+if [[ $SV_SUPER_OPTIONS == *BUILD_OPENCASCADE* ]]; then
+  echo "BUILD_OPENCASCADE"
+  tar xvf Originals/opencascade/opencascade-7.0.0.tgz
+  mv opencascade-7.0.0 ..
+  source Patches/patch-source-opencascade-7.0.0.sh
+fi
+
+# mmg
+if [[ $SV_SUPER_OPTIONS == *BUILD_MMG* ]]; then
+  echo "BUILD_MMG"
+  tar xvf Originals/mmg/mmg-5.1.0.tar.gz
+  mv mmg-5.1.0 ..
+  source Patches/patch-source-mmg-5.1.0.sh
+fi
 
 # mitk
-tar xvf Originals/mitk/mitk-v2016.03.0.tar.gz
-#cd ..
-mv mitk-2016.03 ..
-
-# patches
-source Patches/patch-source-tcltk-8.5.sh
-source Patches/patch-source-vtk-6.2.sh
-source Patches/patch-source-vtk-6.3.sh
-source Patches/patch-source-mmg-5.1.0.sh
-source Patches/patch-source-opencascade-7.0.0.sh
-source Patches/patch-source-mitk-2016.03.sh
+if [[ $SV_SUPER_OPTIONS == *BUILD_MITK* ]]; then
+  echo "BUILD_MITK"
+  tar xvf Originals/mitk/mitk-v2016.03.0.tar.gz
+  #cd ..
+  mv mitk-2016.03 ..
+  source Patches/patch-source-mitk-2016.03.sh
+fi
