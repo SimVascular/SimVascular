949086d181ba6555f802ebff43a8b48fb2030184
osmsc <nwilson@osmsc.com>
2017-01-28 14:23:12 -0800

    DEV: update permissions on linux build script

--- Externals/ubuntu-14-prep.sh
+++ Externals/ubuntu-14-prep.sh
@@ -10,9 +10,9 @@ sudo apt-get install dos2unix
 sudo apt-get install g++
 sudo apt-get install gfortran
 
-### cmake tools
-sudo apt-get install cmake
-sudo apt-get install cmake-qt-gui
+### cmake tools  (note: we need newer version of cmake installed below!)
+#sudo apt-get install cmake
+#sudo apt-get install cmake-qt-gui
 
 ### for flowsolver
 sudo apt-get install libmpich2-dev
@@ -25,6 +25,21 @@ sudo apt-get install libxi-dev
 ### for qt/mitk
  sudo apt-get install libicu-dev
 
+### mitk
+sudo apt-get install libXmu-dev
+sudo apt-get install libXi-dev
+sudo apt-get install libtiff4-dev
+sudo apt-get install libwrap0-dev
+
+### python
+sudo apt-get install libssl-dev
+
+### gdcm/mitk
+sudo apt-get install swig3.0
+
+# optional: mitk
+sudo apt-get install doxygen
+
 ### install Qt
 wget http://simvascular.stanford.edu/downloads/public/open_source/linux/qt/5.4/qt-opensource-linux-x64-5.4.2.run
 chmod a+rx ./qt-opensource-linux-x64-5.4.2.run
@@ -33,7 +48,10 @@ sudo ./qt-opensource-linux-x64-5.4.2.run
 ### install latest version of CMake
 wget http://simvascular.stanford.edu/downloads/public/open_source/linux/cmake/cmake-3.6.1-Linux-x86_64.sh
 chmod a+rx ./cmake-3.6.1-Linux-x86_64.sh
-sudo mkdir /usr/local/package
+sudo mkdir -p /usr/local/package
 sudo ./cmake-3.6.1-Linux-x86_64.sh --prefix=/usr/local/package
-sudo ln -s /usr/local/package/cmake-3.6.1-Linux-x86_64/bin/cmake /usr/local/bin/cmake
+sudo ln -s /usr/local/package/cmake-3.6.1-Linux-x86_64/bin/ccmake    /usr/local/bin/ccmake
+sudo ln -s /usr/local/package/cmake-3.6.1-Linux-x86_64/bin/cmake     /usr/local/bin/cmake
 sudo ln -s /usr/local/package/cmake-3.6.1-Linux-x86_64/bin/cmake-gui /usr/local/bin/cmake-gui
+sudo ln -s /usr/local/package/cmake-3.6.1-Linux-x86_64/bin/cpack     /usr/local/bin/cpack
+sudo ln -s /usr/local/package/cmake-3.6.1-Linux-x86_64/bin/ctest     /usr/local/bin/ctest
