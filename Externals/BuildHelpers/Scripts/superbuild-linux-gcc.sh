8c49418ac8f3662cd6feace3209b3097db566da8
osmsc <nwilson@osmsc.com>
2017-01-29 18:44:56 -0800

    BUG: need to source scripts to create tar and zip files so env variables are picked up

--- Externals/BuildHelpers/Scripts/superbuild-linux-gcc.sh
+++ Externals/BuildHelpers/Scripts/superbuild-linux-gcc.sh
@@ -27,6 +27,7 @@ if [ -z "$SV_SUPER_OPTIONS" ]; then
    SV_SUPER_OPTIONS="WGET_OPENCASCADE UNTAR_OPENCASCADE BUILD_OPENCASCADE ARCHIVE_OPENCASCADE ZIP_OPENCASCADE $SV_SUPER_OPTIONS"
    SV_SUPER_OPTIONS="WGET_MMG         UNTAR_MMG         BUILD_MMG         ARCHIVE_MMG         ZIP_MMG         $SV_SUPER_OPTIONS"
    SV_SUPER_OPTIONS="WGET_MITK        UNTAR_MITK        BUILD_MITK        ARCHIVE_MITK        ZIP_MITK        $SV_SUPER_OPTIONS"
+   export SV_SUPER_OPTIONS
 fi
 
 #
@@ -201,7 +202,7 @@ fi
 # create tar files for distrution
 #
 
-./tmp/create-archives-all.gcc.sh >& ./tmp/stdout.create-archives-all.gcc.txt
+source ./tmp/create-archives-all.gcc.sh >& ./tmp/stdout.create-archives-all.gcc.txt
 
-./tmp/tar-to-zip-all.gcc.sh >& ./tmp/stdout.tar-to-zip-all.gcc.txt
+source ./tmp/tar-to-zip-all.gcc.sh >& ./tmp/stdout.tar-to-zip-all.gcc.txt
 
--- Externals/BuildHelpers/Scripts/superbuild-mac_osx-clang.sh
+++ Externals/BuildHelpers/Scripts/superbuild-mac_osx-clang.sh
@@ -27,6 +27,7 @@ if [ -z "$SV_SUPER_OPTIONS" ]; then
    SV_SUPER_OPTIONS="WGET_OPENCASCADE UNTAR_OPENCASCADE BUILD_OPENCASCADE ARCHIVE_OPENCASCADE ZIP_OPENCASCADE $SV_SUPER_OPTIONS"
    SV_SUPER_OPTIONS="WGET_MMG         UNTAR_MMG         BUILD_MMG         ARCHIVE_MMG         ZIP_MMG         $SV_SUPER_OPTIONS"
    SV_SUPER_OPTIONS="WGET_MITK        UNTAR_MITK        BUILD_MITK        ARCHIVE_MITK        ZIP_MITK        $SV_SUPER_OPTIONS"
+   export SV_SUPER_OPTIONS
 fi
 
 #
@@ -201,7 +202,7 @@ fi
 # create tar files for distrution
 #
 
-./tmp/create-archives-mac_osx.clang.sh >& ./tmp/stdout.create-archives-mac_osx.clang.txt
+source ./tmp/create-archives-mac_osx.clang.sh >& ./tmp/stdout.create-archives-mac_osx.clang.txt
 
-./tmp/tar-to-zip-all.clang.sh >& ./tmp/stdout.tar-to-zip-all.clang.txt
+source ./tmp/tar-to-zip-all.clang.sh >& ./tmp/stdout.tar-to-zip-all.clang.txt
 
