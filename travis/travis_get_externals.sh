4bcf9c9065409b926759ac6bdbc530c4692456b2
osmsc <nwilson@osmsc.com>
2017-03-28 13:01:33 -0700

    BUILD: update externals to 2017.01.29

--- travis/travis_get_externals.sh
+++ travis/travis_get_externals.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PARENT_URL=http://simvascular.stanford.edu/downloads/public/simvascular/externals/linux/ubuntu_14/gnu-4.8/2016.12.11
+export PARENT_URL=http://simvascular.stanford.edu/downloads/public/simvascular/externals/linux/ubuntu_14/gnu-4.8/2017.01.29
 
 sudo mkdir -p $EXTERNALS_TOP
 sudo chmod -R a+rwx $EXTERNALS_TOP
@@ -8,15 +8,15 @@ mkdir -p $EXTERNALS_TOP/tarfiles
 mkdir -p $EXTERNALS_TOP/bin/gnu-4.8/x64
 
 pushd $EXTERNALS_TOP/tarfiles
-wget $PARENT_URL/linux.gnu-4.8.x64.freetype-2.6.3-BUILD2016-12-11.tar.gz
-wget $PARENT_URL/linux.gnu-4.8.x64.gdcm-2.6.1-BUILD2016-12-11.tar.gz
-wget $PARENT_URL/linux.gnu-4.8.x64.itk-4.7.1-BUILD2016-12-11.tar.gz
-wget $PARENT_URL/linux.gnu-4.8.x64.mitk-2016.03-BUILD2016-12-11.tar.gz
-wget $PARENT_URL/linux.gnu-4.8.x64.mmg-5.1.0-BUILD2016-12-11.tar.gz
-wget $PARENT_URL/linux.gnu-4.8.x64.opencascade-7.0.0-BUILD2016-12-11.tar.gz
-wget $PARENT_URL/linux.gnu-4.8.x64.python-2.7.11-BUILD2016-12-11.tar.gz
-wget $PARENT_URL/linux.gnu-4.8.x64.tcltk-8.6.4-BUILD2016-12-11.tar.gz
-wget $PARENT_URL/linux.gnu-4.8.x64.vtk-6.2.0-BUILD2016-12-11.tar.gz
+wget $PARENT_URL/linux.gnu-4.8.x64.freetype-2.6.3-BUILD2017-01-29.tar.gz
+wget $PARENT_URL/linux.gnu-4.8.x64.gdcm-2.6.1-BUILD2017-01-29.tar.gz
+wget $PARENT_URL/linux.gnu-4.8.x64.itk-4.7.1-BUILD2017-01-29.tar.gz
+wget $PARENT_URL/linux.gnu-4.8.x64.mitk-2016.03-BUILD2017-01-29.tar.gz
+wget $PARENT_URL/linux.gnu-4.8.x64.mmg-5.1.0-BUILD2017-01-29.tar.gz
+wget $PARENT_URL/linux.gnu-4.8.x64.opencascade-7.0.0-BUILD2017-01-29.tar.gz
+wget $PARENT_URL/linux.gnu-4.8.x64.python-2.7.11-BUILD2017-01-29.tar.gz
+wget $PARENT_URL/linux.gnu-4.8.x64.tcltk-8.6.4-BUILD2017-01-29.tar.gz
+wget $PARENT_URL/linux.gnu-4.8.x64.vtk-6.2.0-BUILD2017-01-29.tar.gz
 popd
 
 pushd $EXTERNALS_TOP/bin/gnu-4.8/x64
