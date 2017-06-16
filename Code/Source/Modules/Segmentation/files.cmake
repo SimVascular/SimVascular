06d880cb9437b66c076f561ad08ede5aa3deea00
nwilson <nwilson@osmsc.com>
2017-06-16 13:40:16 -0700

    BUG: needed to remove \'s

--- Code/Source/Modules/Segmentation/files.cmake
+++ Code/Source/Modules/Segmentation/files.cmake
@@ -1,66 +1,66 @@
 set(H_FILES
-    svSegmentationUtils.h \
-    svContour.h \
-    svContourCircle.h \
-    svContourEllipse.h \
-    svContourPolygon.h \
-    svContourTensionPolygon.h \
-    svContourSplinePolygon.h \
-    svContourOperation.h \
-    svContourModel.h \
-    svContourModelVtkMapper2D.h \
-    svContourModelThresholdInteractor.h \
-    svContourGroup.h \
-    svContourGroupVtkMapper2D.h \
-    svContourGroupVtkMapper3D.h \
-    svContourGroupDataInteractor.h \
-    svContourGroupIO.h \
-    svSegmentationLegacyIO.h \
-    svSurface.h \
-    svSurfaceVtkMapper3D.h \
-    svSeg3D.h \
-    svMitkSeg3D.h \
-    svMitkSeg3DOperation.h \
-    svMitkSeg3DIO.h \
-    svMitkSeg3DVtkMapper3D.h \
-    svMitkSeg3DDataInteractor.h \
-    svSegmentationObjectFactory.h \
+    svSegmentationUtils.h
+    svContour.h
+    svContourCircle.h
+    svContourEllipse.h
+    svContourPolygon.h
+    svContourTensionPolygon.h
+    svContourSplinePolygon.h
+    svContourOperation.h
+    svContourModel.h
+    svContourModelVtkMapper2D.h
+    svContourModelThresholdInteractor.h
+    svContourGroup.h
+    svContourGroupVtkMapper2D.h
+    svContourGroupVtkMapper3D.h
+    svContourGroupDataInteractor.h
+    svContourGroupIO.h
+    svSegmentationLegacyIO.h
+    svSurface.h
+    svSurfaceVtkMapper3D.h
+    svSeg3D.h
+    svMitkSeg3D.h
+    svMitkSeg3DOperation.h
+    svMitkSeg3DIO.h
+    svMitkSeg3DVtkMapper3D.h
+    svMitkSeg3DDataInteractor.h
+    svSegmentationObjectFactory.h
     svSeg3DUtils.h
 )
 
 set(CPP_FILES
-    svSegmentationUtils.cxx \
-    svContour.cxx \
-    svContourCircle.cxx \
-    svContourEllipse.cxx \
-    svContourPolygon.cxx \
-    svContourTensionPolygon.cxx \
-    svContourSplinePolygon.cxx \
-    svContourOperation.cxx \
-    svContourModel.cxx \
-    svContourModelVtkMapper2D.cxx \
-    svContourModelThresholdInteractor.cxx \
-    svContourGroup.cxx \
-    svContourGroupVtkMapper2D.cxx \
-    svContourGroupVtkMapper3D.cxx \
-    svContourGroupDataInteractor.cxx \
-    svContourGroupIO.cxx \
-    svSegmentationLegacyIO.cxx \
-    svSurface.cxx \
-    svSurfaceVtkMapper3D.cxx \
-    svSeg3D.cxx \
-    svMitkSeg3D.cxx \
-    svMitkSeg3DOperation.cxx \
-    svMitkSeg3DIO.cxx \
-    svMitkSeg3DVtkMapper3D.cxx \
-    svMitkSeg3DDataInteractor.cxx \
-    svSegmentationObjectFactory.cxx \
+    svSegmentationUtils.cxx
+    svContour.cxx
+    svContourCircle.cxx
+    svContourEllipse.cxx
+    svContourPolygon.cxx
+    svContourTensionPolygon.cxx
+    svContourSplinePolygon.cxx
+    svContourOperation.cxx
+    svContourModel.cxx
+    svContourModelVtkMapper2D.cxx
+    svContourModelThresholdInteractor.cxx
+    svContourGroup.cxx
+    svContourGroupVtkMapper2D.cxx
+    svContourGroupVtkMapper3D.cxx
+    svContourGroupDataInteractor.cxx
+    svContourGroupIO.cxx
+    svSegmentationLegacyIO.cxx
+    svSurface.cxx
+    svSurfaceVtkMapper3D.cxx
+    svSeg3D.cxx
+    svMitkSeg3D.cxx
+    svMitkSeg3DOperation.cxx
+    svMitkSeg3DIO.cxx
+    svMitkSeg3DVtkMapper3D.cxx
+    svMitkSeg3DDataInteractor.cxx
+    svSegmentationObjectFactory.cxx
     svSeg3DUtils.cxx
 )
 
 set(RESOURCE_FILES
-    Interactions/svContourGroupInteraction.xml \
-    Interactions/svContourModelThresholdInteraction.xml \
-    Interactions/svMitkSeg3DInteraction.xml \
+    Interactions/svContourGroupInteraction.xml
+    Interactions/svContourModelThresholdInteraction.xml
+    Interactions/svMitkSeg3DInteraction.xml
     Interactions/svSegmentationConfig.xml
 )
--- Code/Source/Plugins/org.sv.gui.qt.segmentation/files.cmake
+++ Code/Source/Plugins/org.sv.gui.qt.segmentation/files.cmake
@@ -3,44 +3,44 @@ set(SRC_CPP_FILES
 )
 
 set(INTERNAL_CPP_FILES
-    svLoftingUtils.cxx \
-    svContourGroupCreate.cxx \
-    svContourGroupCreateAction.cxx \
-    svSegmentationLegacyLoadAction.cxx \
-    svSegmentationLegacySaveAction.cxx \
-    svSegmentationLoadAction.cxx \
-    svLevelSet2DWidget.cxx \
-    svLoftParamWidget.cxx \
-    svSeg2DEdit.cxx \
-    svContourGroupPoint2DSizeAction.cxx \
-    svContourGroupPoint3DSizeAction.cxx \
-    svSeg3DCreateAction.cxx \
-    svSeg3DEdit.cxx \
+    svLoftingUtils.cxx
+    svContourGroupCreate.cxx
+    svContourGroupCreateAction.cxx
+    svSegmentationLegacyLoadAction.cxx
+    svSegmentationLegacySaveAction.cxx
+    svSegmentationLoadAction.cxx
+    svLevelSet2DWidget.cxx
+    svLoftParamWidget.cxx
+    svSeg2DEdit.cxx
+    svContourGroupPoint2DSizeAction.cxx
+    svContourGroupPoint3DSizeAction.cxx
+    svSeg3DCreateAction.cxx
+    svSeg3DEdit.cxx
     svSegmentationPluginActivator.cxx
 )
 
 set(MOC_H_FILES
-    src/internal/svLoftingUtils.h \
-    src/internal/svContourGroupCreate.h \
-    src/internal/svContourGroupCreateAction.h \
-    src/internal/svSegmentationLegacyLoadAction.h \
-    src/internal/svSegmentationLegacySaveAction.h \
-    src/internal/svSegmentationLoadAction.h \
-    src/internal/svLevelSet2DWidget.h \
-    src/internal/svLoftParamWidget.h \
-    src/internal/svSeg2DEdit.h \
-    src/internal/svContourGroupPoint2DSizeAction.h \
-    src/internal/svContourGroupPoint3DSizeAction.h \
-    src/internal/svSeg3DCreateAction.h \
-    src/internal/svSeg3DEdit.h \
+    src/internal/svLoftingUtils.h
+    src/internal/svContourGroupCreate.h
+    src/internal/svContourGroupCreateAction.h
+    src/internal/svSegmentationLegacyLoadAction.h
+    src/internal/svSegmentationLegacySaveAction.h
+    src/internal/svSegmentationLoadAction.h
+    src/internal/svLevelSet2DWidget.h
+    src/internal/svLoftParamWidget.h
+    src/internal/svSeg2DEdit.h
+    src/internal/svContourGroupPoint2DSizeAction.h
+    src/internal/svContourGroupPoint3DSizeAction.h
+    src/internal/svSeg3DCreateAction.h
+    src/internal/svSeg3DEdit.h
     src/internal/svSegmentationPluginActivator.h
 )
 
 set(UI_FILES
-    src/internal/svContourGroupCreate.ui \
-    src/internal/svLevelSet2DWidget.ui \
-    src/internal/svLoftParamWidget.ui \
-    src/internal/svSeg2DEdit.ui \
+    src/internal/svContourGroupCreate.ui
+    src/internal/svLevelSet2DWidget.ui
+    src/internal/svLoftParamWidget.ui
+    src/internal/svSeg2DEdit.ui
     src/internal/svSeg3DEdit.ui
 )
 
