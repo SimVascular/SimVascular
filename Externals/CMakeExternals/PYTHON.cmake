c52042e9c27fa6f6eb04e5b4f7fb1ebe499ee815
updega2 <updega2@berkeley.edu>
2018-02-13 14:05:02 -0800

    DEV: Updating install script with crypto and ssl rpath changes

--- Externals/CMakeExternals/PYTHON.cmake
+++ Externals/CMakeExternals/PYTHON.cmake
@@ -84,7 +84,6 @@ if(APPLE)
   set(SV_EXTERNALS_${proj}_INSTALL_SCRIPT install-python-mac_osx.sh)
   configure_file(${SV_EXTERNALS_CMAKE_DIR}/Install/${SV_EXTERNALS_${proj}_INSTALL_SCRIPT}.in "${SV_EXTERNALS_${proj}_BIN_DIR}/${SV_EXTERNALS_${proj}_INSTALL_SCRIPT}" @ONLY)
   set(SV_EXTERNALS_${proj}_CUSTOM_INSTALL make install
-    COMMAND install_name_tool -id @rpath/${${proj}_LIBRARY_NAME} ${SV_EXTERNALS_${proj}_LIBRARY}
     COMMAND ${SV_EXTERNALS_${proj}_BIN_DIR}/${SV_EXTERNALS_${proj}_INSTALL_SCRIPT})
 elseif(LINUX)
   list(APPEND SV_EXTERNALS_${proj}_ADDITIONAL_CMAKE_ARGS
