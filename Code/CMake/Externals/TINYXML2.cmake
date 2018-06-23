784ef723fef775c0ee922e890a3e50a8876d9436
osmsc <nwilson@osmsc.com>
2018-06-23 13:21:16 -0700

    BUILD: differ location for tinyxml2 depending on platform

--- Code/CMake/Externals/TINYXML2.cmake
+++ Code/CMake/Externals/TINYXML2.cmake
@@ -38,7 +38,11 @@ if(SV_USE_${proj})
   # If using toplevel dir, TINYXML2_DIR to be the SV_TINYXML2_DIR set by the
   # simvascular_add_new_external macro
   if(SV_EXTERNALS_USE_TOPLEVEL_BIN_DIR)
-      set(${proj}_DIR ${SV_${proj}_DIR}/lib/cmake/tinyxml2 CACHE PATH "Force ${proj} dir to externals" FORCE)
+      if(APPLE)
+        set(${proj}_DIR ${SV_${proj}_DIR}/share/lib/cmake/tinyxml2 CACHE PATH "Force ${proj} dir to externals" FORCE)
+      else()
+        set(${proj}_DIR ${SV_${proj}_DIR}/lib/cmake/tinyxml2 CACHE PATH "Force ${proj} dir to externals" FORCE)
+      endif()
       if(WIN32)
         set(${proj}_DLL_PATH "${SV_${proj}_DIR}/bin" CACHE PATH "Force Tinyxml2 DLL Path" FORCE)
       endif()
