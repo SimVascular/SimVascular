09cfe54747462d1c5c8cf86640cec881c06e7c4d
root <root@ip-10-10-30-138.ec2.internal>
2019-03-25 23:12:53 +0000

    DEV: temporary update for amazon to installing qt

--- Externals/Make/2019.02/BuildHelpers/CompileScripts/install-qt-linux.sh
+++ Externals/Make/2019.02/BuildHelpers/CompileScripts/install-qt-linux.sh
@@ -4,14 +4,12 @@ sudo rm -Rf REPLACEME_SV_TOP_BIN_DIR_QT
 mkdir -p REPLACEME_SV_TOP_BIN_DIR_QT
 chmod u+w,a+rx REPLACEME_SV_TOP_BIN_DIR_QT
 
-echo ""
-echo "*** install in ****"
-echo ""
-echo "REPLACEME_SV_TOP_BIN_DIR_QT"
-echo ""
-echo "*******************"
-echo ""
+wget http://simvascular.stanford.edu/downloads/public/simvascular/externals/2019.02/linux/centos/7.6/gnu/6.3/x64/release/2019.03.16/centos.7.6.gnu.6.3.x64.release.2019.03.16.qt.5.6.3.tar.gz
 
-sudo ./Originals/qt/qt-opensource-linux-x64-5.6.3.run --script tmp/qt.installer-noninteractive.qs
+tar xzf ./centos.7.6.gnu.6.3.x64.release.2019.03.16.qt.5.6.3.tar.gz
+
+mv ./qt-5.6.3/* REPLACEME_SV_TOP_BIN_DIR_QT
+
 sudo chown -R $USER REPLACEME_SV_TOP_BIN_DIR_QT
 sudo chgrp -R $USER REPLACEME_SV_TOP_BIN_DIR_QT
+
