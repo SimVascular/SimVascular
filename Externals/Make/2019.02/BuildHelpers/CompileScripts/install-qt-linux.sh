c7d528e80aaff3d73a12fc72e37f758200a4077f
simvascular <simvascular@localhost.localdomain>
2019-03-28 16:02:39 -0700

    DEV: updated linux bin qt installer to work on all 4 platforms

--- Externals/Make/2019.02/BuildHelpers/CompileScripts/install-qt-linux.sh
+++ Externals/Make/2019.02/BuildHelpers/CompileScripts/install-qt-linux.sh
@@ -4,12 +4,40 @@ sudo rm -Rf REPLACEME_SV_TOP_BIN_DIR_QT
 mkdir -p REPLACEME_SV_TOP_BIN_DIR_QT
 chmod u+w,a+rx REPLACEME_SV_TOP_BIN_DIR_QT
 
-wget http://simvascular.stanford.edu/downloads/public/simvascular/externals/2019.02/linux/centos/7.6/gnu/6.3/x64/release/2019.03.16/centos.7.6.gnu.6.3.x64.release.2019.03.16.qt.5.6.3.tar.gz
+echo ""
+echo "*** install in ****"
+echo ""
+echo "REPLACEME_SV_TOP_BIN_DIR_QT"
+echo ""
+echo "*******************"
+echo ""
 
-tar xzf ./centos.7.6.gnu.6.3.x64.release.2019.03.16.qt.5.6.3.tar.gz
+osid=$(lsb_release -si)
+case "$osid" in
 
-mv ./qt-5.6.3/* REPLACEME_SV_TOP_BIN_DIR_QT
+  'Ubuntu')
+     sudo ./Originals/qt/qt-opensource-linux-x64-5.6.3.run --script tmp/qt.installer-noninteractive.qs
+     ;;
+
+  'CentOS')
+     sudo ./Originals/qt/qt-opensource-linux-x64-5.6.3.run --script tmp/qt.installer-noninteractive.qs
+     ;;
+
+  'AmazonAMI')
+     tar xzf ./Originals/qt/qt-opensource-centos-x64-5.6.3.tar.gz
+     mv 5.6.3 REPLACEME_SV_TOP_BIN_DIR_QT 
+     ;;
+
+  'Amazon')
+     tar xzf ./Originals/qt/qt-opensource-centos-x64-5.6.3.tar.gz
+     mv 5.6.3 REPLACEME_SV_TOP_BIN_DIR_QT  
+     ;;
+
+  *)	 
+    echo "Error!  Invalid Linux Version!"
+    exit
+    ;;
+esac
 
 sudo chown -R $USER REPLACEME_SV_TOP_BIN_DIR_QT
 sudo chgrp -R $USER REPLACEME_SV_TOP_BIN_DIR_QT
-
