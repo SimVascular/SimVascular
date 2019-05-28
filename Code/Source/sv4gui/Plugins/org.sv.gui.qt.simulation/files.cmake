c262dba63b855c9b6a04536076981b9cf20c724f
osmsc <nwilson@osmsc.com>
2019-05-27 19:23:57 -0700

    DEV: split the MPI detection into a seperate preferences tab in the simulation plugin

--- Code/Source/sv4gui/Plugins/org.sv.gui.qt.simulation/files.cmake
+++ Code/Source/sv4gui/Plugins/org.sv.gui.qt.simulation/files.cmake
@@ -37,12 +37,14 @@ set(CPP_FILES
     sv4gui_TableSolverDelegate.cxx
     sv4gui_SimulationView.cxx
     sv4gui_SimulationPreferencePage.cxx
+    sv4gui_MPIPreferencePage.cxx
     sv4gui_SimJobStopAction.cxx
     sv4gui_SimJobExportAction.cxx
     sv4gui_SimulationPluginActivator.cxx
     sv4gui_ProcessHandler.cxx
     sv4gui_SolverProcessHandler.cxx
     sv4gui_SimulationPreferences.cxx
+    sv4gui_MPIPreferences.cxx
 )
 
 set(MOC_H_FILES
@@ -54,12 +56,14 @@ set(MOC_H_FILES
     sv4gui_TableSolverDelegate.h
     sv4gui_SimulationView.h
     sv4gui_SimulationPreferencePage.h
+    sv4gui_MPIPreferencePage.h
     sv4gui_SimJobStopAction.h
     sv4gui_SimJobExportAction.h
     sv4gui_SimulationPluginActivator.h
     sv4gui_ProcessHandler.h
     sv4gui_SolverProcessHandler.h
     sv4gui_SimulationPreferences.h
+    sv4gui_MPIPreferences.h
 )
 
 set(UI_FILES
@@ -68,6 +72,7 @@ set(UI_FILES
     sv4gui_SplitBCWidget.ui
     sv4gui_SimulationPreferencePage.ui
     sv4gui_SimulationView.ui
+    sv4gui_MPIPreferencePage.ui
 )
 
 set(CACHED_RESOURCE_FILES
---
+++ Code/Source/sv4gui/Plugins/org.sv.gui.qt.simulation/sv4gui_MPIPreferences.cxx
@@ -0,0 +1,228 @@
+/* Copyright (c) Stanford University, The Regents of the University of
+ *               California, and others.
+ *
+ * All Rights Reserved.
+ *
+ * See Copyright-SimVascular.txt for additional details.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "sv4gui_MPIPreferences.h"
+
+#include <QDir>
+#include <QFile>
+#include <QFileInfo>
+#include <QProcess>
+
+#include <iostream>
+
+// Set the string value representing an unresolved binary.
+const QString sv4guiMPIPreferences::UnknownBinary("not found");
+
+//-----------------------------
+// sv4guiMPIPreferences
+//-----------------------------
+//
+sv4guiMPIPreferences::sv4guiMPIPreferences() 
+{
+  m_MpiEnumToString = { 
+    {MpiImplementation::MPICH, "MPICH"},
+    {MpiImplementation::OpenMPI, "OpenMPI"},
+    {MpiImplementation::Unknown, "Unknown"}
+  };
+
+  m_MpiStringToEnum = {
+    {"MPICH", MpiImplementation::MPICH}, 
+    {"OpenMPI", MpiImplementation::OpenMPI},
+    {"Unknown", MpiImplementation::Unknown}
+  };
+
+  InitializeMPILocation();
+}
+
+sv4guiMPIPreferences::~sv4guiMPIPreferences()
+{
+}
+
+//---------------------------
+// InitializeMPILocation
+//---------------------------
+// Find the location of solver binaries and mpiexec.
+//
+// The the full binary path is displayed in the SimVascular 
+// 'Preferences->SimVascular Simulations' page and used to 
+//execute a simulation.
+//
+void sv4guiMPIPreferences::InitializeMPILocation()
+{
+  // Set the default install location of the solver.
+  QString solverInstallPath = "/usr/local/sv/svsolver";
+  QStringList dirList = QDir(solverInstallPath).entryList(QDir::Dirs|QDir::NoDotAndDotDot|QDir::NoSymLinks,QDir::Name);
+  if (dirList.size() != 0) {
+    solverInstallPath += "/" + dirList.back();
+  }
+
+  // Set the path to the SimVascular-build/bin directory.
+  QString applicationPath = "";
+  //QString applicationPath = QCoreApplication::applicationDirPath();
+
+  // Set the mpiexec binary.
+  SetMpiExec(solverInstallPath, applicationPath); 
+
+  // Set the MPI implementation. 
+  SetMpiImplementation();
+}
+
+//------------
+// SetMpiExec
+//------------
+// Set the location of the MPI mpiexec binary.
+//
+// Don't display a warning until the solver is actually used. 
+//
+void sv4guiMPIPreferences::SetMpiExec(const QString& solverPath, const QString& applicationPath)
+{
+  QString mpiExec = UnknownBinary;
+  QString mpiExecPath;
+  QString filePath = "";
+  QString mpiExecName = "mpiexec";
+
+#if defined(Q_OS_LINUX)
+
+  mpiExecPath = "/usr/bin/";
+
+  if (QFile(filePath = mpiExecPath + mpiExecName).exists()) {
+    mpiExec = filePath;
+  }
+
+#elif defined(Q_OS_MAC)
+
+  mpiExecPath = "/usr/local/bin/";
+
+  if (QFile(filePath = mpiExecPath + mpiExecName).exists()) {
+    mpiExec = filePath;
+  }
+
+#elif defined(Q_OS_WIN)
+
+    QString msmpiDir = GetRegistryValue("Microsoft\\MPI","InstallRoot");
+
+    if (msmpiDir != "") {
+      if (msmpiDir.endsWith("\\")) {
+        mpiExec = msmpiDir+"Bin\\mpiexec";
+      } else {
+        mpiExec = msmpiDir+"\\Bin\\mpiexec";
+      }
+    }
+
+#endif
+
+  m_mpiExec = mpiExec;
+}
+
+QString sv4guiMPIPreferences::GetMpiExec() 
+{ 
+  return m_mpiExec; 
+}
+
+//----------------------
+// SetMpiImplementation 
+//----------------------
+// Set the installed MPI implementation.
+//
+// This is needed to unsure that MPI is installed and that the
+// correct implementation is installed for a given OS.
+//
+// Check the implementation using 'mpiexec -version' and show
+// it in the Preferences panel.
+//
+// Another way to check the MPI implementation is to use 'mpicc -show'
+// but it is probably better to check the actual mpiexec that will be 
+// used to run jobs.
+//
+void sv4guiMPIPreferences::SetMpiImplementation()
+{
+  MpiImplementation implementation = MpiImplementation::Unknown;
+  m_MpiImplementation = implementation; 
+
+  if (m_mpiExec == UnknownBinary) {
+      return;
+  }
+
+  QFileInfo fileInfo(m_mpiExec);
+  QString mpiExecPath = fileInfo.path();
+
+#if defined(Q_OS_LINUX) || defined(Q_OS_MAC)
+  QProcess *checkMpi = new QProcess();
+  QString program(m_mpiExec);
+
+  if (QFile(program).exists()) {
+    QStringList arguments;
+    arguments << "-version";
+    checkMpi->setProgram(program);
+    checkMpi->setArguments(arguments);
+    checkMpi->start(program, arguments);
+    checkMpi->waitForFinished(); 
+    QString output(checkMpi->readAllStandardOutput());
+    if (output.contains("mpich")) {
+      implementation = MpiImplementation::MPICH;
+    } else if (output.contains("OpenRTE")) {
+      implementation = MpiImplementation::OpenMPI;
+    } else {
+      implementation = MpiImplementation::Unknown;
+    }
+  }
+
+#elif defined(Q_OS_WIN)
+
+#endif
+
+  m_MpiImplementation = implementation;
+}
+
+//------------
+// GetMpiName
+//------------
+// Get the name of the MPI implementation.
+//
+const QString sv4guiMPIPreferences::GetMpiName()
+{
+  return m_MpiEnumToString[m_MpiImplementation];
+}
+
+sv4guiMPIPreferences::MpiImplementation
+sv4guiMPIPreferences::GetMpiImplementation()
+{
+  return m_MpiImplementation;
+}
+
+sv4guiMPIPreferences::MpiImplementation 
+sv4guiMPIPreferences::GetMpiImplementation(const QString& name)
+{
+  if (m_MpiStringToEnum.find(name) == m_MpiStringToEnum.end()) {
+      return sv4guiMPIPreferences::MpiImplementation::Unknown;
+  }
+  return m_MpiStringToEnum[name];
+}
