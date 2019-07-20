670c785219b52b7f0988f1136cefd21c1d94e888
osmsc <nwilson@osmsc.com>
2019-07-19 18:40:29 -0700

    DEV: updated SV zlib with mitk zlib

--- Code/ThirdParty/zlib/simvascular_zlib/simvascular_zlib_mangle.h
+++ Code/ThirdParty/zlib/simvascular_zlib/simvascular_zlib_mangle.h
@@ -1,13 +1,21 @@
 #ifndef simvascular_zlib_mangle_h
 #define simvascular_zlib_mangle_h
 /*
+
 This header file mangles all symbols exported from the zlib library.
 It is included in all files while building the zlib library.  Due to
 namespace pollution, no zlib headers should be included in .h files in
-simvascular. It is based on the VTK header file with a similar name.
+cm.
 
-This was used (on OSX) to generate this list:
-nm /usr/lib/libz.dylib | grep " [TRD] " | awk '{ print "#define "substr($3,2)" cv_zlib_"substr($3,2) }'
+The following command was used to obtain the symbol list:
+
+nm libcmzlib.so |grep " [TRD] "
+
+This is the way to recreate the whole list:
+
+nm libcmzlib.so |grep " [TRD] " | awk '{ print "#define "$3" cv_zlib_"$3 }'
+
+REMOVE the "_init" and "_fini" entries.
 
 */
 
@@ -18,6 +26,7 @@ nm /usr/lib/libz.dylib | grep " [TRD] " | awk '{ print "#define "substr($3,2)" c
 #define compressBound cv_zlib_compressBound
 #define crc32 cv_zlib_crc32
 #define crc32_combine cv_zlib_crc32_combine
+#define get_crc_table cv_zlib_get_crc_table
 #define deflate cv_zlib_deflate
 #define deflateBound cv_zlib_deflateBound
 #define deflateCopy cv_zlib_deflateCopy
@@ -30,12 +39,9 @@ nm /usr/lib/libz.dylib | grep " [TRD] " | awk '{ print "#define "substr($3,2)" c
 #define deflateSetDictionary cv_zlib_deflateSetDictionary
 #define deflateSetHeader cv_zlib_deflateSetHeader
 #define deflateTune cv_zlib_deflateTune
-#define get_crc_table cv_zlib_get_crc_table
-#define gzbuffer cv_zlib_gzbuffer
+#define deflate_copyright cv_zlib_deflate_copyright
 #define gzclearerr cv_zlib_gzclearerr
 #define gzclose cv_zlib_gzclose
-#define gzclose_r cv_zlib_gzclose_r
-#define gzclose_w cv_zlib_gzclose_w
 #define gzdirect cv_zlib_gzdirect
 #define gzdopen cv_zlib_gzdopen
 #define gzeof cv_zlib_gzeof
@@ -43,7 +49,6 @@ nm /usr/lib/libz.dylib | grep " [TRD] " | awk '{ print "#define "substr($3,2)" c
 #define gzflush cv_zlib_gzflush
 #define gzgetc cv_zlib_gzgetc
 #define gzgets cv_zlib_gzgets
-#define gzoffset cv_zlib_gzoffset
 #define gzopen cv_zlib_gzopen
 #define gzprintf cv_zlib_gzprintf
 #define gzputc cv_zlib_gzputc
@@ -55,25 +60,32 @@ nm /usr/lib/libz.dylib | grep " [TRD] " | awk '{ print "#define "substr($3,2)" c
 #define gztell cv_zlib_gztell
 #define gzungetc cv_zlib_gzungetc
 #define gzwrite cv_zlib_gzwrite
+#define inflate_fast cv_zlib_inflate_fast
 #define inflate cv_zlib_inflate
-#define inflateBack cv_zlib_inflateBack
-#define inflateBackEnd cv_zlib_inflateBackEnd
-#define inflateBackInit_ cv_zlib_inflateBackInit_
 #define inflateCopy cv_zlib_inflateCopy
 #define inflateEnd cv_zlib_inflateEnd
 #define inflateGetHeader cv_zlib_inflateGetHeader
 #define inflateInit2_ cv_zlib_inflateInit2_
 #define inflateInit_ cv_zlib_inflateInit_
-#define inflateMark cv_zlib_inflateMark
 #define inflatePrime cv_zlib_inflatePrime
 #define inflateReset cv_zlib_inflateReset
-#define inflateReset2 cv_zlib_inflateReset2
 #define inflateSetDictionary cv_zlib_inflateSetDictionary
 #define inflateSync cv_zlib_inflateSync
 #define inflateSyncPoint cv_zlib_inflateSyncPoint
-#define inflateUndermine cv_zlib_inflateUndermine
+#define inflate_copyright cv_zlib_inflate_copyright
+#define inflate_table cv_zlib_inflate_table
+#define _dist_code cv_zlib__dist_code
+#define _length_code cv_zlib__length_code
+#define _tr_align cv_zlib__tr_align
+#define _tr_flush_block cv_zlib__tr_flush_block
+#define _tr_init cv_zlib__tr_init
+#define _tr_stored_block cv_zlib__tr_stored_block
+#define _tr_tally cv_zlib__tr_tally
 #define uncompress cv_zlib_uncompress
 #define zError cv_zlib_zError
+#define z_errmsg cv_zlib_z_errmsg
+#define zcalloc cv_zlib_zcalloc
+#define zcfree cv_zlib_zcfree
 #define zlibCompileFlags cv_zlib_zlibCompileFlags
 #define zlibVersion cv_zlib_zlibVersion
 
