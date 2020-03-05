81fc5d09ce311c01e2d96e7f68a67afad1ab5be8
Dave Parker <davep@stanford.edu>
2020-03-04 18:20:54 -0800

    FIx an ordering problem.

--- Code/Source/sv4gui/Modules/Model/Common/sv4gui_ModelVtkMapper3D.h
+++ Code/Source/sv4gui/Modules/Model/Common/sv4gui_ModelVtkMapper3D.h
@@ -50,6 +50,14 @@
 #include <vtkActor.h>
 #include <vtkPlaneCollection.h>
 #include <vtkSmartPointer.h>
+#include <tuple>
+
+// Define a tuple for storing face mapper/actor in a map for reuse.
+//
+// The bool element is a flag used to identify if a mapper/actor is
+// no longer being used.
+//
+typedef std::tuple<vtkOpenGLPolyDataMapper*, vtkActor*, bool> FaceMapperActor;
 
 /* Properties that can be set for surfaces and influence the sv4guiModelVtkMapper3D are:
   *
@@ -161,6 +169,12 @@ protected:
 
     virtual void CheckForClippingProperty( mitk::BaseRenderer* renderer, mitk::BaseProperty *property );
 
+private:
+    // Define a map for storing face mapper/actor.
+    std::map<vtkSmartPointer<vtkPolyData>, FaceMapperActor> faceMapperActor; 
+    void GetFaceMapperAndActor(sv4guiModelElement::svFace* face, vtkOpenGLPolyDataMapper** faceMapper, vtkActor** faceActor);
+    void ResetFaceMapperAndActor();
+    void UpdateFaceMapperAndActor();
 };
 
 
