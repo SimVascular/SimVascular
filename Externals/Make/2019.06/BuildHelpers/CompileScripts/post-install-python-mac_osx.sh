6f41e943f646a6cf6a0545b85037a874a3d7c10c
osmsc <nwilson@osmsc.com>
2021-02-01 15:06:28 -0800

    DEV: adding python packages needed by 1d code to externals

--- Externals/Make/2019.06/BuildHelpers/CompileScripts/post-install-python-mac_osx.sh
+++ Externals/Make/2019.06/BuildHelpers/CompileScripts/post-install-python-mac_osx.sh
@@ -11,17 +11,28 @@ GRM=rm
 # need pip to install things
 REPLACEME_SV_TOP_BIN_DIR_PYTHON/bin/python REPLACEME_SV_TOPLEVEL_SRCDIR/BuildHelpers/Originals/python/get-pip.py
 
+# purge if a previous installation of python existed
+REPLACEME_SV_TOP_BIN_DIR_PYTHON/bin/pip cache purge
+
 # needed to compile numpy if desired
 REPLACEME_SV_TOP_BIN_DIR_PYTHON/bin/pip install Cython --install-option="--no-cython-compile"
 
 #must install torando-5.1.1 since tornado-6.0.1 breaks jupyter!!
-REPLACEME_SV_TOP_BIN_DIR_PYTHON/bin/pip install tornado==5.1.1
+REPLACEME_SV_TOP_BIN_DIR_PYTHON/Scripts/pip.exe install tornado==5.1.1
 #install numpy
-REPLACEME_SV_TOP_BIN_DIR_PYTHON/bin/pip install numpy==1.14.3
+REPLACEME_SV_TOP_BIN_DIR_PYTHON/Scripts/pip.exe install numpy==1.14.3
 #install jupyter
-REPLACEME_SV_TOP_BIN_DIR_PYTHON/bin/pip install jupyter
+REPLACEME_SV_TOP_BIN_DIR_PYTHON/Scripts/pip.exe install jupyter==1.0.0
 #install tensorflow
-REPLACEME_SV_TOP_BIN_DIR_PYTHON/bin/pip install tensorflow==1.4.0
+REPLACEME_SV_TOP_BIN_DIR_PYTHON/Scripts/pip.exe install tensorflow==1.14.0
+#install pwlf for 1-D python code
+REPLACEME_SV_TOP_BIN_DIR_PYTHON/Scripts/pip.exe install pwlf==2.0.4
+#install scipy for 1-D python code
+REPLACEME_SV_TOP_BIN_DIR_PYTHON/Scripts/pip.exe install scipy==1.2.0
+#install pydoe for 1-D python code
+REPLACEME_SV_TOP_BIN_DIR_PYTHON/Scripts/pip.exe install pydoe==0.3.8
+#install pyyaml needed for ML
+REPLACEME_SV_TOP_BIN_DIR_PYTHON/Scripts/pip.exe install pyyaml==3.12
 
 $GMKDIR REPLACEME_SV_TOP_BIN_DIR_PYTHON/bin-relocate
 
