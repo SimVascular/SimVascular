d8fcc09bb58ab00c3aaaeacc17eb538280ea76eb
osmsc <nwilson@osmsc.com>
2021-02-01 23:02:11 -0800

    DEV: fixed script to install python site-packages on linux and macos_x

--- Externals/Make/2019.06/BuildHelpers/CompileScripts/post-install-python-linux.sh
+++ Externals/Make/2019.06/BuildHelpers/CompileScripts/post-install-python-linux.sh
@@ -18,21 +18,21 @@ REPLACEME_SV_TOP_BIN_DIR_PYTHON/bin/pip cache purge
 REPLACEME_SV_TOP_BIN_DIR_PYTHON/bin/pip install Cython --install-option="--no-cython-compile"
 
 #must install torando-5.1.1 since tornado-6.0.1 breaks jupyter!!
-REPLACEME_SV_TOP_BIN_DIR_PYTHON/Scripts/pip.exe install tornado==5.1.1
+REPLACEME_SV_TOP_BIN_DIR_PYTHON/bin/pip install tornado==5.1.1
 #install numpy
-REPLACEME_SV_TOP_BIN_DIR_PYTHON/Scripts/pip.exe install numpy==1.14.3
+REPLACEME_SV_TOP_BIN_DIR_PYTHON/bin/pip install numpy==1.14.3
 #install jupyter
-REPLACEME_SV_TOP_BIN_DIR_PYTHON/Scripts/pip.exe install jupyter==1.0.0
+REPLACEME_SV_TOP_BIN_DIR_PYTHON/bin/pip install jupyter==1.0.0
 #install tensorflow
-REPLACEME_SV_TOP_BIN_DIR_PYTHON/Scripts/pip.exe install tensorflow==1.14.0
+REPLACEME_SV_TOP_BIN_DIR_PYTHON/bin/pip install tensorflow==1.14.0
 #install pwlf for 1-D python code
-REPLACEME_SV_TOP_BIN_DIR_PYTHON/Scripts/pip.exe install pwlf==2.0.4
+REPLACEME_SV_TOP_BIN_DIR_PYTHON/bin/pip install pwlf==2.0.4
 #install scipy for 1-D python code
-REPLACEME_SV_TOP_BIN_DIR_PYTHON/Scripts/pip.exe install scipy==1.2.0
+REPLACEME_SV_TOP_BIN_DIR_PYTHON/bin/pip install scipy==1.2.0
 #install pydoe for 1-D python code
-REPLACEME_SV_TOP_BIN_DIR_PYTHON/Scripts/pip.exe install pydoe==0.3.8
+REPLACEME_SV_TOP_BIN_DIR_PYTHON/bin/pip install pydoe==0.3.8
 #install pyyaml needed for ML
-REPLACEME_SV_TOP_BIN_DIR_PYTHON/Scripts/pip.exe install pyyaml==3.12
+REPLACEME_SV_TOP_BIN_DIR_PYTHON/bin/pip install pyyaml==3.12
 
 $GMKDIR REPLACEME_SV_TOP_BIN_DIR_PYTHON/bin-relocate
 
