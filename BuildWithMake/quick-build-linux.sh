fc7b5a9ed707fb185837e214f0d14822f2f7de5a
osmsc <nwilson@osmsc.com>
2021-06-09 19:03:41 -0700

    DEV: fixed make build for PythonAPI
    BUILD: adding focal into quick build

--- BuildWithMake/quick-build-linux.sh
+++ BuildWithMake/quick-build-linux.sh
@@ -46,13 +46,13 @@ case "$osid" in
 	export EXTERNALS_SV_OS_LONG_NAME_DIR=ubuntu
 	export EXTERNALS_SV_COMPILER_SHORT_NAME=gnu
 	case "$osver" in
-	    'disco')
-		# use 18.04 externals for now
-		export SV_EXTERN_LINUX_VERSION=ubuntu_19
-		export EXTERNALS_SV_FULL_OS_VER_NO=19.04
-		export EXTERNALS_SV_COMPILER_VER_NO=8.3
+	    'focal')
+		# use 18.04 externals for now for 20.04
+		export SV_EXTERN_LINUX_VERSION=ubuntu_18
+		export EXTERNALS_SV_FULL_OS_VER_NO=18.04
+		export EXTERNALS_SV_COMPILER_VER_NO=7.3
 		if [ $SV_EXTERNALS_VERSION_NUMBER == '2019.02' ]; then
-		  echo "Error - no 2019.02 for ubuntu 19!"
+		  echo "Error - no 2019.02 for ubuntu 20!"
 		  exit
 		elif [ $SV_EXTERNALS_VERSION_NUMBER == '2019.06' ]; then
                   export EXTERNALS_BUILD_DATE=2019.07.22
