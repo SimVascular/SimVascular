46d32adc8416dcb59c7c334a6a576567aa541335
Jakob <jakob.richter@tum.de>
2022-06-05 13:25:53 -0700

    use vs enterprise

--- BuildWithMake/startX-with-2017.bat
+++ BuildWithMake/startX-with-2017.bat
@@ -7,7 +7,7 @@ REM set the correct path and visual studio path if you have Intel Fortran compil
 REM call "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2019.4.245\windows\bin\compilervars.bat" intel64 vs2017
 
 REM Add in MSVC 2017 C++ x64 compiler (not needed if you include ifort above)
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 
 REM use startxwin if you use x server, otherwise just use next line
 REM bash --login
