b4796d5993f3b38d629f0472a38a5dfd5b687b47
Jakob <jakob.richter@tum.de>
2022-06-05 01:21:01 -0700

    Use old bat file again

--- BuildWithMake/startX-with-2017.bat
+++ BuildWithMake/startX-with-2017.bat
@@ -1,1 +1,14 @@
+@echo off
+
+C:
+chdir C:/cygwin64/bin
+
+REM set the correct path and visual studio path if you have Intel Fortran compiler
+REM call "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2019.4.245\windows\bin\compilervars.bat" intel64 vs2017
+
+REM Add in MSVC 2017 C++ x64 compiler (not needed if you include ifort above)
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+REM use startxwin if you use x server, otherwise just use next line
+REM bash --login
+bash --login
