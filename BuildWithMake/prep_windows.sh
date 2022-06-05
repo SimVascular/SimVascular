bb9c3f6f0ebc17f8f9f485ce409443b7d847996c
Jakob <jakob.richter@tum.de>
2022-06-04 22:45:41 -0700

    Add bat and shell scripts

---
+++ BuildWithMake/prep_windows.sh
@@ -0,0 +1,16 @@
+#
+# alias cd=cd_func
+
+if hash CL 2>/dev/null; then
+    clfullpath=`which CL`
+    clparentdir="$(dirname "$clfullpath")"
+    export PATH=$clparentdir::$PATH
+fi
+if hash vsstrace.exe 2>/dev/null; then
+    rcfullpath=`which vsstrace.exe`
+    rcparentdir="$(dirname "$rcfullpath")"
+    export PATH=$rcparentdir:$PATH
+fi
+
+# add my custom dir to path
+export PATH=~/bin:$PATH
