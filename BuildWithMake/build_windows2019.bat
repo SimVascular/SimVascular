095f8edc1cbc56bcb2264dd324edf1454dc0865e
Jakob <jakob.richter@tum.de>
2022-06-05 19:08:49 -0700

    Delete vs 2019 before installation of new one

--- BuildWithMake/build_windows2019.bat
+++ BuildWithMake/build_windows2019.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 
 sh ./quick-build-windows.sh
 
