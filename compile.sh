03934407aca1dc7e022c57500b4867857b1962dd
Luca Pegolotti <pegolotti.luca@gmail.com>
2023-06-19 10:14:32 -0700

    Add compile scripts + change VTK and gdcm versions accordin to mitk specifications

---
+++ compile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+build_dir=build2
+rm -rf build2
+mkdir $build_dir
+
+# Python libraries are not found by the interpreter
+export LD_LIBRARY_PATH=$(pwd)/$build_dir/Externals-build/svExternals/bin/python-3.9.10/lib
+
+cd $build_dir
+cmake ..
+make -j 8
